128e4a8d0a85251a0d2ccb4a6f681c59a2aee803
kleberquaresma <kleberquaresma@outlook.com>
2022-11-29 21:33:25 -0300

    .

--- BOOTCAMP/Cloud Fullstack Bootcamp - Warburg Pincus/TRILHA/2-Primeiras páginas iterativas com JavaScript/EXEMPLOS E EXERCÍCIOS/Aula JavaScript DIO - 9.2-Integrando com a PokeAPI/assets/js/main.js
+++ BOOTCAMP/Cloud Fullstack Bootcamp - Warburg Pincus/TRILHA/2-Primeiras páginas iterativas com JavaScript/EXEMPLOS E EXERCÍCIOS/Aula JavaScript DIO - 9.2-Integrando com a PokeAPI/assets/js/main.js
@@ -1,6 +1,4 @@
-const offset=0
-const limit=10
-const url=`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+// Função base para criação da lista de Pokemons.
 
 function convertPokemonToLi(pokemon){
     return `
@@ -20,18 +18,79 @@ function convertPokemonToLi(pokemon){
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => {
+
+// EXEMPLO 01
+
+pokeApi.getPokemons().then((pokemons = []) => {
 
         const listItems = []
 
         for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i]
 
-           // pokemonList.innerHTML +=   convertPokemonToLi(pokemon) // Aqui concatena item a item, o que funciona, mas não é o ideal. Melhor concatenar tudo de uma vez.
+           //pokemonList.innerHTML +=   convertPokemonToLi(pokemon) // Aqui concatena item a item, o que funciona, mas não é o ideal. Melhor concatenar tudo de uma vez através de um Array.
 
            listItems.push(convertPokemonToLi(pokemon))
         }
 
+        console.log(listItems)
+
     })
     .catch((error) => console.log(error))
     .finally(() => console.log('Requisição concluída'))
+
+    
+// EXEMPLO 02: utilizando o MAP. Resultado é semelhante ao item anterior, só que está retornando apenas os nomes dos Pokeons.
+
+pokeApi.getPokemons().then((pokemons = []) => {
+
+    const newList = pokemons.map((pokemon) => {
+        return pokemon.name
+    })
+
+    console.log(newList)
+
+})
+.catch((error) => console.log(error))
+.finally(() => console.log('Requisição concluída'))
+
+
+// EXEMPLO 03: utilizando o MAP. Resultado é o mesmo do EXEMPLO 01, só que com código reduzido, sem pecisar usar o FOR, substintuindo-o pelo MAP.
+
+pokeApi.getPokemons().then((pokemons = []) => {
+
+    const newList = pokemons.map((pokemon) => {
+        return convertPokemonToLi(pokemon)
+    })
+
+    console.log(newList)
+
+})
+.catch((error) => console.log(error))
+.finally(() => console.log('Requisição concluída'))
+
+
+// EXEMPLO 04: utilizando o MAP. Juntando os itens da lista para exibição na página.
+
+pokeApi.getPokemons().then((pokemons = []) => {
+
+    const newList = pokemons.map(convertPokemonToLi) // Quando a Arro Funtion tem apenas uma linha pode-se retirar {}, deixando o código ainda mais reduzido. Além disso, no MAP já pode ser passado direto o resultado da função "convertPokemonToLi".
+
+    const newHtml = newList.join('') // O "join" junta os itens e as aspas não está indicando separador entre os itens.
+
+    pokemonList.innerHTML += newHtml // Cria o novo HTML e exibe os pokemons em lista. O "+=" concatena.
+
+})
+.catch((error) => console.log(error))
+.finally(() => console.log('Requisição concluída'))
+
+
+// EXEMPLO 05: utilizando o MAP. Reduzindo ainda mais o código.
+
+pokeApi.getPokemons().then((pokemons = []) => {
+
+    pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('') // Aqui o "=" está substituindo o HTML pelo novo resultado.
+
+})
+.catch((error) => console.log(error))
+.finally(() => console.log('Requisição concluída'))
--- BOOTCAMP/Cloud Fullstack Bootcamp - Warburg Pincus/TRILHA/2-Primeiras páginas iterativas com JavaScript/EXEMPLOS E EXERCÍCIOS/Aula JavaScript DIO - 9.2-Integrando com a PokeAPI/assets/js/poke-api.js
+++ BOOTCAMP/Cloud Fullstack Bootcamp - Warburg Pincus/TRILHA/2-Primeiras páginas iterativas com JavaScript/EXEMPLOS E EXERCÍCIOS/Aula JavaScript DIO - 9.2-Integrando com a PokeAPI/assets/js/poke-api.js
@@ -10,3 +10,15 @@ const pokeApi = {} // Criação do objeto pokeApi
         .then((jsonBody) => jsonBody.results)
         .catch((error) => console.log(error))
     }
+
+
+    //EXEMPLO 01: Usar um "Promise" para "esperar" pelo resultado de requisições e conseguirmos pega os detalhes dos Pokemons.
+
+    Promise.all([
+        fetch('https://pokeapi.co/api/v2/pokemon/1/'),
+        fetch('https://pokeapi.co/api/v2/pokemon/2/'),
+        fetch('https://pokeapi.co/api/v2/pokemon/3/'),
+        fetch('https://pokeapi.co/api/v2/pokemon/4/'),
+    ]).then((results) => {
+        console.log(results)
+    })
