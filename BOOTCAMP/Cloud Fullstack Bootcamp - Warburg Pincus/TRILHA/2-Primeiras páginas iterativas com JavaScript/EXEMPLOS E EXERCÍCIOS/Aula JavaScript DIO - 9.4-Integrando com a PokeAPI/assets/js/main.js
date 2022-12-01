ed8ef177a9c38994d1c49eee4be00cdb03468191
kleberquaresma <kleberquaresma@outlook.com>
2022-12-01 20:51:51 -0300

    .

--- BOOTCAMP/Cloud Fullstack Bootcamp - Warburg Pincus/TRILHA/2-Primeiras páginas iterativas com JavaScript/EXEMPLOS E EXERCÍCIOS/Aula JavaScript DIO - 9.4-Integrando com a PokeAPI/assets/js/main.js
+++ BOOTCAMP/Cloud Fullstack Bootcamp - Warburg Pincus/TRILHA/2-Primeiras páginas iterativas com JavaScript/EXEMPLOS E EXERCÍCIOS/Aula JavaScript DIO - 9.4-Integrando com a PokeAPI/assets/js/main.js
@@ -1,19 +1,15 @@
-// Funções base para criação da lista de Pokemons.
-
-function convertPokemonTypeToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
+// Função base para criação da lista de Pokemons.
 
 function convertPokemonToLi(pokemon){
     return `
-            <li class="pokemon">
-                <spam class="number">${pokemon.order}</spam>
+            <li class="pokemon ${pokemon.type}">
+                <spam class="number">${pokemon.number}</spam>
                 <spam class="name">${pokemon.name}</spam>
                 <div class="detail">
                     <ol class="types">
-                        ${convertPokemonTypeToLi(pokemon.types).join('')}
+                        ${pokemon.types.map((type) => `li class="type">${type}</li>`).join('')}
                     </ol>
-                    <img src="${pokemon.sprites.other.dream_world.front_default}" alt='${pokemon.name}'>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
                 </div>
             </li>
             `
